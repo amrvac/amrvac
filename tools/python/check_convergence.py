c207603fc95ef60ffbcd43d9552bd317ecb6a7b3
Jannis Teunissen <jannis@teunissen.net>
2018-01-31 19:02:12 +0100

    Improve check_convergence.py
    - can now select variable to plot
    - runs amrvac in parallel
    - plots with different markers/styles

--- tools/python/check_convergence.py
+++ tools/python/check_convergence.py
@@ -5,6 +5,7 @@ import re
 import numpy as np
 import sys
 import argparse
+import itertools
 
 def get_args():
     # Get and parse the command line arguments
@@ -18,15 +19,14 @@ def get_args():
 
     p.add_argument('-par', type=str, default='amrvac.par',
                    help='Which par files to use')
-
+    p.add_argument('-var', type=str, default='err_1',
+                   help='Which variable to consider')
     p.add_argument('-limiters', type=str, nargs='+', default=['koren'],
                    help='Which limiter(s) to include')
     p.add_argument('-schemes', type=str, nargs='+', default=['hll'],
                    help='Which flux scheme(s) to include')
     p.add_argument('-integrators', type=str, nargs='+', default=['threestep'],
                    help='Which time integrators(s) to include')
-    p.add_argument('-norm', type=str, default='2', choices=['1', '2', 'inf'],
-                   help='Which norm to use (1, 2, inf)')
     p.add_argument('-powers', type=float, nargs='+', default=[],
                    help='Show these powers in the graph')
     p.add_argument('-iprob', type=int, default=1,
@@ -35,10 +35,14 @@ def get_args():
                    help='Test problem dimension')
     p.add_argument('-nx', type=int, default=1024,
                    help='Maximum number of grid points')
-    p.add_argument('-nxlow', type=int, default=16,
+    p.add_argument('-nxlow', type=int, default=32,
                    help='Minimum number of grid points')
+    p.add_argument('-nxblock', type=int, default=16,
+                   help='Size of grid blocks')
     p.add_argument('-fig', type=str,
                    help='Store a figure with this name (e.g., test.pdf)')
+    p.add_argument('-np', type=int, default=4,
+                   help='Number of CPUs to use (with mpirun)')
     return p.parse_args()
 
 
@@ -53,6 +57,15 @@ if __name__ == '__main__':
     p = get_args()
 
     all_results = []
+
+    if p.nxlow % p.nxblock != 0:
+        print("nxblock={} does not divide nxlow={}".format(
+            p.nxblock, p.nxlow))
+        sys.exit(1)
+
+    if p.nx < p.nxlow:
+        print("Error: nx={} smaller than nxlow={}".format(p.nx, p.nxlow))
+        sys.exit(1)
 
     # Number of grids to use
     ngrids = int(np.log2(float(p.nx)/p.nxlow) + 1.5)
@@ -71,7 +84,7 @@ if __name__ == '__main__':
                 nx_results = np.zeros(ngrids,)
 
                 for i, nx in enumerate(nx_list):
-                    with open('this_scheme.par', 'w') as f:
+                    with open('CONV_TEST.par', 'w') as f:
                        f.write('! Temporary par file\n')
                        f.write('&methodlist\n')
                        f.write(' time_integrator = "{}"\n'.format(ti))
@@ -80,40 +93,48 @@ if __name__ == '__main__':
                        f.write('/\n')
                        f.write('&meshlist\n')
                        f.write(' domain_nx1 = {}\n'.format(nx))
-                       f.write(' block_nx1 = {}\n'.format(p.nxlow))
+                       f.write(' block_nx1 = {}\n'.format(p.nxblock))
                        if p.dim > 1:
                            f.write(' domain_nx2 = {}\n'.format(nx))
-                           f.write(' block_nx2 = {}\n'.format(p.nxlow))
+                           f.write(' block_nx2 = {}\n'.format(p.nxblock))
                        if p.dim > 2:
                            f.write(' domain_nx3 = {}\n'.format(nx))
-                           f.write(' block_nx3 = {}\n'.format(p.nxlow))
+                           f.write(' block_nx3 = {}\n'.format(p.nxblock))
                        f.write(' iprob = {}\n'.format(p.iprob))
                        f.write('/\n')
 
                     try:
-                        res = check_output(["./amrvac", "-i", p.par,
-                                            "this_scheme.par"])
+                        res = check_output(["mpirun", "-np", str(p.np),
+                                            "./amrvac", "-i", p.par,
+                                            "CONV_TEST.par"])
                         res = res.decode(encoding="utf-8")
                     except CalledProcessError as e:
                         print("\nAMRVAC returned an error")
                         print(e.output)
                         sys.exit(1)
 
-                    # Catch line before "Total timeloop took"
-                    mobj = re.search(r'^(.*)$\n^\s*Total timeloop took',
-                                     res, re.MULTILINE)
+                    # Catch last line starting with CONVTEST
+                    line = re.findall(r'^\s*CONVTEST.*$', res, re.MULTILINE)[-1]
 
-                    # Split the string which contains the errors
-                    errors = mobj.group(1).split()
+                    # Format should be CONVTEST (var1,var2,...): val1 val2 val3
+                    # Extract variable names and values
+                    i0 = line.find('(')
+                    i1 = line.find(')')
+                    names = line[i0+1:i1].lstrip()
+                    names = re.split(r'[ ,\t\n]+', names)
+                    i0 = line.find(':')
+                    vals = line[i0+1:-1].lstrip()
+                    vals = re.split(r'[ ,\t\n]+', vals)
+                    vals = map(float, vals)
 
-                    # Extract the appropriate norm
-                    if p.norm == '1':
-                        nx_results[i] = float(errors[-3])
-                    elif p.norm == '2':
-                        nx_results[i] = float(errors[-2])
-                    else:
-                        nx_results[i] = float(errors[-1])
+                    try:
+                        i0 = names.index(p.var)
+                    except:
+                        print("{} not in list of variables: {}".format(
+                            p.var, ' '.join(names)))
+                        sys.exit(1)
 
+                    nx_results[i] = vals[i0]
                     n_done += 1
                     progress_bar((100.0 * n_done) / n_results)
 
@@ -124,15 +145,21 @@ if __name__ == '__main__':
         import matplotlib.pyplot as plt
 
         plt.figure(figsize=[10.,6.])
+        marker = itertools.cycle(('+', '.', 'x', '*', 'v',
+                                  '^', '<', '>', '8'))
+        linestyle = itertools.cycle((':', '-.', '--', '-'))
+
+        mean_val = 0.0
         for res in all_results:
             lbl = '-'.join(res[0:3])
-            line, = plt.loglog(nx_list, res[3], linestyle='-',
-                               marker='.', label=lbl)
+            line, = plt.loglog(nx_list, res[3], linestyle=linestyle.next(),
+                               marker=marker.next(), label=lbl)
+            mean_val = mean_val + res[3][0] / len(all_results)
 
         for pp in p.powers:
-            plt.loglog(nx_list, (float(nx_list[0])/nx_list)**pp,
+            plt.loglog(nx_list, mean_val * (float(nx_list[0])/nx_list)**pp,
                        linestyle='--', label='order-{:.1f}'.format(pp))
-        plt.ylabel('Error {}-norm'.format(p.norm))
+        plt.ylabel('Error {}'.format(p.var))
         plt.xlabel('Grid points')
         plt.legend(loc='lower left')
         plt.xlim([nx_list[0]*0.9, nx_list[-1]*1.1])
