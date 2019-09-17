6d50d3429dd8b677bb6effd5575436b5818187ab
Niels Claes <niels.claes@kuleuven.be>
2019-09-17 16:02:53 +0200

    cleanup python tools (3): remove 'deprecated' folder containing old tools

--- tools/deprecated/python/old/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("lic_internal", ["lic_internal.pyx"])]
-)
-
